2b34f66d0d9145db59d19fc6b06ababf6c68077e
Vasili Pl <w2e3r4t51982@gmail.com>
2025-10-05 11:15:00 +0200

    working version

--- src/test/java/JsonPlaceholderTests.java
+++ src/test/java/JsonPlaceholderTests.java
@@ -1,9 +1,8 @@
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.*;
+import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.*;
 
 public class JsonPlaceholderTests {
@@ -26,28 +25,58 @@ public class JsonPlaceholderTests {
     }
 
     @Test
+    @DisplayName("Получаем пользователя по ID")
+    public void getUserByIdTest() {
+        RestAssured.baseURI = BASE_URL;
+
+        given()
+                .when()
+                .get("/users/4")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("$", not(empty()));
+    }
+
+    @Test
+    @DisplayName("Получаем пользователя по неверному ID")
+    public void getUserByWrongIdTest() {
+        RestAssured.baseURI = BASE_URL;
+
+        given()
+                .when()
+                .get("/users/24")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(404)
+                .body("$", not(empty()));
+    }
+
+    @Test
     @DisplayName("GET post by ID")
     public void getPostByIdTest() {
         RestAssured.baseURI = BASE_URL;
 
         given()
                 .when()
-                .get("/posts/1")
+                .get("/posts/20")
                 .then()
                 .log().status()
                 .log().body()
                 .statusCode(200)
-                .body("id", is(1));
+                .body("id", is(20));
     }
 
     @Test
-    @DisplayName("POST create new post")
+    @DisplayName("Создание нового поста")
     public void createPostTest() {
         RestAssured.baseURI = BASE_URL;
 
         given()
-                .contentType(ContentType.JSON)
-                .body("{\"title\": \"New post\", \"body\": \"Post body\", \"userId\": 1}")
+                .contentType(JSON)
+                .body("{\"title\": \"Basil post\", \"body\": \"Fata viam invenient.\", \"userId\": 1}")
                 .when()
                 .post("/posts")
                 .then()
@@ -55,28 +84,28 @@ public class JsonPlaceholderTests {
                 .log().body()
                 .statusCode(201)
                 .body("id", notNullValue())
-                .body("title", is("New post"));
+                .body("title", is("Basil post"));
     }
 
     @Test
-    @DisplayName("PUT update post")
+    @DisplayName("Изменение поста")
     public void updatePostTest() {
         RestAssured.baseURI = BASE_URL;
 
         given()
-                .contentType(ContentType.JSON)
-                .body("{\"id\": 1, \"title\": \"Updated post\", \"body\": \"Updated body\", \"userId\": 1}")
+                .contentType(JSON)
+                .body("{\"id\": 1, \"title\": \"Basil post updated\", \"body\": \"Fata viam invenient.(пер. «Судьба найдёт путь.»)\", \"userId\": 1}")
                 .when()
                 .put("/posts/1")
                 .then()
                 .log().status()
                 .log().body()
                 .statusCode(200)
-                .body("title", is("Updated post"));
+                .body("title", is("Basil post updated"));
     }
 
     @Test
-    @DisplayName("DELETE post")
+    @DisplayName("Удаление поста")
     public void deletePostTest() {
         RestAssured.baseURI = BASE_URL;
 
